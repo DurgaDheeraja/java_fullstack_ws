e6f4be6b82364866e87d7c2287dd56baf31bbd89
DurgaDheeraja <badugudheeraja@gmail.com>
2023-12-21 10:26:52 -0500

    extra command is added

--- src/main/java/com/amazon/Main.java
+++ src/main/java/com/amazon/Main.java
@@ -2,6 +2,9 @@ package com.amazon;
 
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hello world!");
+        System.out.println("Welcome to java program");
     }
 }
